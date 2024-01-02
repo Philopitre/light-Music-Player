6474468c9dc475fffd4a4d87b587a2bfe8582458
Philow <92055523+Philopitre@users.noreply.github.com>
2024-01-03 00:55:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -123,13 +123,13 @@ const playNextSong = () => {
 };
 
 const playPreviousSong = () =>{
-   if (userData?.currentSong === null) return;
-   else {
-    const currentSongIndex = getCurrentSongIndex();
-    const previousSong = userData?.songs[currentSongIndex - 1];
+  if (userData?.currentSong === null) return;
+  else {
+  const currentSongIndex = getCurrentSongIndex();
+  const previousSong = userData?.songs[currentSongIndex - 1];
 
-    playSong(previousSong.id);
-   }
+  playSong(previousSong.id);
+  }
 };
 
 const shuffle = () => {
